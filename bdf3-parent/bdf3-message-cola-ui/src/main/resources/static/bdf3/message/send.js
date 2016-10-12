0aac9d5dae07ea40b3ecb48c2d10a21f75145e39
muxiangqiu <kevin.yang@bstek.com>
2016-10-12 15:23:57 +0800

    发送私信接收者可以过滤和选择

--- bdf3-parent/bdf3-message-cola-ui/src/main/resources/static/bdf3/message/send.js
+++ bdf3-parent/bdf3-message-cola-ui/src/main/resources/static/bdf3/message/send.js
@@ -2,9 +2,34 @@
     cola(function (model) {
 
         var service = {
-            add: "../api/message/add"
+            add: "../api/message/add",
+            loadUsers: "../service/user/load"
         };
 
+//        $.fn.form.settings.rules.adminLevel = function(value) {
+//            return value && model.get("message.conten")
+//        };
+
+
+        tinymce.init({
+            selector: '#txtMessage',
+            height: 250,
+            theme: 'modern',
+            plugins: [
+                'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                'searchreplace wordcount visualblocks visualchars code fullscreen',
+                'insertdatetime media nonbreaking save table contextmenu directionality',
+                'emoticons template paste textcolor colorpicker textpattern imagetools'
+            ],
+            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons',
+            image_advtab: true,
+            menubar: false
+        }).then(function(editors) {
+            editors[0].on("keyup", function() {
+                var content = tinymce.get("txtMessage").getContent()
+                model.set("message.content", content);
+            });
+        });
 
         $("#messageForm").form({
             on: "blur",
@@ -30,10 +55,32 @@
             }
         });
 
+        model.describe("users", {
+            provider: {
+                url: service.loadUsers,
+                pageSize: 18,
+                parameter: {
+                    searchKey: "{{searchKey}}"
+                },
+                success: function() {
+                    $("#ddUser").visibility({
+                        once: true,
+                        observeChanges: true,
+                        onBottomVisible: function() {
+                            model.get("users").nextPage();
+                        }
+                    });
+                }
+            }
+        });
+
 
         model.set("message", {});
 
         model.action({
+            search: function () {
+                return model.flush("users");
+            },
             openListPage: function() {
                 if (typeof window.parent.expandAndOpenPage === "function") {
                     window.parent.expandAndOpenPage("message");
@@ -68,6 +115,12 @@
             },
             cancel: function() {
                 model.action("openListPage")();
+            }
+        });
+
+        $(".ui.dropdown").dropdown({
+            onChange: function(value, text, $selectedItem) {
+                model.set("message.additional", value);
             }
         });
     });
