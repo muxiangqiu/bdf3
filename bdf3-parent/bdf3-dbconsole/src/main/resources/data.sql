9cfc97d87f7aeeca90d195244b0a1b2afc729af2
muxiangqiu <kevin.yang@bstek.com>
2017-02-11 01:38:55 +0800

    修改数据库管理菜单图标

--- bdf3-parent/bdf3-dbconsole/src/main/resources/data.sql
+++ bdf3-parent/bdf3-dbconsole/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO BDF3_URL (DESCRIPTION_, NAME_, ICON_, PATH_, NAVIGABLE_, ORDER_, PARENT_ID_, ID_) VALUES(NULL,'数据库管理','fa fa-book yellow-text','bdf3.dbconsole.view.DbConsoleMaintain.d',1,9,NULL,'cafb93bb-15d6-4737-aec0-0d500aaeb8e5')
+INSERT INTO BDF3_URL (DESCRIPTION_, NAME_, ICON_, PATH_, NAVIGABLE_, ORDER_, PARENT_ID_, ID_) VALUES(NULL,'数据库管理','fa fa-database blue-text','bdf3.dbconsole.view.DbConsoleMaintain.d',1,9,NULL,'cafb93bb-15d6-4737-aec0-0d500aaeb8e5')
