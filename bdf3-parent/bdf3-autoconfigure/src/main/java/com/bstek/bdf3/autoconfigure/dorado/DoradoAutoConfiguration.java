ada60e7d3c421324f7feab195e5f10de17760e60
kevin <kevin@192.168.18.204>
2017-10-10 17:45:42 +0800

    1.依赖dorado-core-7.6.0-SNAPSHOT 2.为dorado的相关静态资源文件添加浏览器缓存支持，dorado的静态资源默认缓存一年，可以通过spring.resources.cache-period属性来自定用过期时间

--- bdf3-parent/bdf3-autoconfigure/src/main/java/com/bstek/bdf3/autoconfigure/dorado/DoradoAutoConfiguration.java
+++ bdf3-parent/bdf3-autoconfigure/src/main/java/com/bstek/bdf3/autoconfigure/dorado/DoradoAutoConfiguration.java
@@ -1,14 +1,35 @@
 package com.bstek.bdf3.autoconfigure.dorado;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.CacheControl;
 
 import com.bstek.bdf3.dorado.configure.DoradoConfiguration;
+import com.bstek.dorado.web.resolver.AbstractResolver;
 import com.bstek.dorado.web.servlet.DoradoServlet;
 
 /**
@@ -19,7 +40,11 @@ import com.bstek.dorado.web.servlet.DoradoServlet;
 @ConditionalOnClass(DoradoConfiguration.class)
 @AutoConfigureAfter(EmbeddedServletContainerAutoConfiguration.class)
 @Import(DoradoConfiguration.class)
-public class DoradoAutoConfiguration {
+public class DoradoAutoConfiguration implements ApplicationContextAware {
+	
+	@Autowired
+	private ResourceProperties resourceProperties;
+
 	
 	
 	@Bean
@@ -28,5 +53,103 @@ public class DoradoAutoConfiguration {
 		servletRegistrationBean.setLoadOnStartup(1);
 		return servletRegistrationBean;
     }
+	
+	@Bean
+	public FilterRegistrationBean someFilterRegistration() {
+
+	    FilterRegistrationBean registration = new FilterRegistrationBean();
+	    registration.setFilter(new Filter() {
+	    	
+	    	private static final String HEADER_PRAGMA = "Pragma";
+
+	    	private static final String HEADER_EXPIRES = "Expires";
+
+	    	private static final String HEADER_CACHE_CONTROL = "Cache-Control";
+
+			@Override
+			public void destroy() {
+				
+			}
+
+			@Override
+			public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+					throws IOException, ServletException {
+				HttpServletResponse httpResponse = (HttpServletResponse) res;
+				Integer cachePeriod = resourceProperties.getCachePeriod();
+				if (cachePeriod == null) {
+					cachePeriod = 31536000;
+				}
+				applyCacheControl(httpResponse, CacheControl.maxAge(cachePeriod, TimeUnit.SECONDS));
+				chain.doFilter(req, res);
+				
+			}
+			
+			private void applyCacheControl(HttpServletResponse response, CacheControl cacheControl) {
+				String ccValue = cacheControl.getHeaderValue();
+				if (ccValue != null) {
+					// Set computed HTTP 1.1 Cache-Control header
+					response.setHeader(HEADER_CACHE_CONTROL, ccValue);
+
+					if (response.containsHeader(HEADER_PRAGMA)) {
+						// Reset HTTP 1.0 Pragma header if present
+						response.setHeader(HEADER_PRAGMA, "");
+					}
+					if (response.containsHeader(HEADER_EXPIRES)) {
+						// Reset HTTP 1.0 Expires header if present
+						response.setHeader(HEADER_EXPIRES, "");
+					}
+				}
+			}
+
+
+			@Override
+			public void init(FilterConfig arg0) throws ServletException {
+				
+			}
+	    	
+	    });
+	    registration.addUrlPatterns("*.dpkg", "/dorado/res/**", "/dorado/client/**");
+	    registration.setOrder(Integer.MAX_VALUE -100);
+	    return registration;
+	} 
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		Map<String, AbstractResolver> map = applicationContext.getBeansOfType(AbstractResolver.class);
+		for (Entry<String, AbstractResolver> entry : map.entrySet()) {
+			entry.getValue().setCacheSeconds(300);;
+		}
+		
+	}
+	
+//	@Configuration
+//	@EnableConfigurationProperties(ResourceProperties.class)
+//	public static class WebMvcConfigurationAdapter extends WebMvcConfigurerAdapter {
+//		private static final Log logger = LogFactory
+//				.getLog(WebMvcConfigurerAdapter.class);
+//
+//		private final ResourceProperties resourceProperties;
+//		
+//		public WebMvcConfigurationAdapter(ResourceProperties resourceProperties) {
+//			this.resourceProperties = resourceProperties;
+//		}
+//		
+//		@Override
+//		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//			if (!this.resourceProperties.isAddMappings()) {
+//				logger.debug("Default resource handling disabled");
+//				return;
+//			}
+//			Integer cachePeriod = this.resourceProperties.getCachePeriod();
+//			if (!registry.hasMappingForPattern("*.dpkg")) {
+//				registry.addResourceHandler("*.dpkg")
+//						.setCachePeriod(cachePeriod);
+//			}
+//		}
+//		
+//		
+//
+//	}
+	
 	
 }
--- bdf3-parent/bdf3-autoconfigure/src/main/java/com/bstek/bdf3/autoconfigure/dorado/DoradoPreloadSpringApplicationRunListener.java
+++ bdf3-parent/bdf3-autoconfigure/src/main/java/com/bstek/bdf3/autoconfigure/dorado/DoradoPreloadSpringApplicationRunListener.java
@@ -9,6 +9,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.util.ClassUtils;
 
+import com.bstek.dorado.core.CommonContext;
+import com.bstek.dorado.core.Context;
 import com.bstek.dorado.web.loader.DoradoLoader;
 
 public class DoradoPreloadSpringApplicationRunListener implements
@@ -27,8 +29,23 @@ public class DoradoPreloadSpringApplicationRunListener implements
 	}
 
 	@Override
-	public void contextPrepared(ConfigurableApplicationContext context) {
-
+	public void contextPrepared(ConfigurableApplicationContext applicationContext) {
+		if (ClassUtils.isPresent("com.bstek.dorado.web.loader.DoradoLoader", this.getClass().getClassLoader())){
+			System.setProperty("doradoHome", "classpath:dorado-home/");
+	
+			DoradoLoader doradoLoader = DoradoLoader.getInstance();
+			try {
+				Context context = CommonContext.init(applicationContext);
+				DoradoLoader.getInstance().setFailSafeContext(context);
+				doradoLoader.preload(true);;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Set<Object> sources = new LinkedHashSet<Object>();
+			sources.addAll(doradoLoader
+					.getContextLocations(false));
+			application.setSources(sources);
+		}
 	}
 
 	@Override
@@ -44,20 +61,6 @@ public class DoradoPreloadSpringApplicationRunListener implements
 
 	@Override
 	public void starting() {
-		if (ClassUtils.isPresent("com.bstek.dorado.web.loader.DoradoLoader", this.getClass().getClassLoader())){
-			System.setProperty("doradoHome", "classpath:dorado-home/");
-	
-			DoradoLoader doradoLoader = DoradoLoader.getInstance();
-			try {
-				doradoLoader.preload(null, true);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			Set<Object> sources = new LinkedHashSet<Object>();
-			sources.addAll(doradoLoader
-					.getContextLocations(false));
-			application.setSources(sources);
-		}
 		
 	}
 
