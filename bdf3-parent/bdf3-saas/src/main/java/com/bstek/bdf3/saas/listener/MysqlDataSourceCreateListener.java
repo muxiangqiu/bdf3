3ddca86012af0511bddaf3fd8b32bac1fe9d34c6
kevin <kevin.yang@codemune.com>
2017-11-07 11:48:40 +0800

    补充主数据库名称映射规则

--- bdf3-parent/bdf3-saas/src/main/java/com/bstek/bdf3/saas/listener/MysqlDataSourceCreateListener.java
+++ bdf3-parent/bdf3-saas/src/main/java/com/bstek/bdf3/saas/listener/MysqlDataSourceCreateListener.java
@@ -1,5 +1,6 @@
 package com.bstek.bdf3.saas.listener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component;
 import com.bstek.bdf3.saas.Constants;
 import com.bstek.bdf3.saas.domain.DataSourceInfo;
 import com.bstek.bdf3.saas.domain.Organization;
+import com.bstek.bdf3.saas.service.DatabaseNameService;
 
 /**
  * @author Kevin Yang (mailto:kevin.yang@bstek.com)
@@ -16,12 +18,15 @@ import com.bstek.bdf3.saas.domain.Organization;
 @Order(1000)
 public class MysqlDataSourceCreateListener implements DataSourceCreateListener {
 
+	@Autowired
+	private DatabaseNameService databaseNameService;
+	
 	@Override
 	public void onCreate(Organization organization, DataSourceInfo dataSourceInfo,
 			DataSourceBuilder dataSourceBuilder) {
 		if ("com.mysql.jdbc.Driver".equals(dataSourceInfo.getDriverClassName())) {
 			String url = dataSourceInfo.getUrl();
-			if (!url.contains(Constants.MASTER)) {
+			if (!url.contains(databaseNameService.getDatabaseName(Constants.MASTER))) {
 				url += "/" + organization.getId();
 				dataSourceBuilder.url(url);
 			}
